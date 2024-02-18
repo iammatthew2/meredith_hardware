ccf200c954a361a63994e1b6d193e9e00168e5d9
Matthew Villeneuve <matthew.villeneuve@gmail.com>
2024-02-18 10:14:05 -0800

    health check & override functional

--- meredith_hardware.ino
+++ meredith_hardware.ino
@@ -1,6 +1,6 @@
 /*
   Meredith is a timed trigger management tool. The timed triggers fire https requests after the trigger has been
-  activated longer than a given threadhold. The timed triggers can be overridden by long-pressing on the big button.
+  activated longer than a given threadhold. The timed triggers can be put into a sleep mode by long-pressing on the big button.
 
   This util is built with two included triggers (trigger_left and trigger_right). More triggers can be added by defining new
   TimedTriggerConfig structures
@@ -10,10 +10,7 @@
 #include "arduino_secrets.h"
 #include "CuteBuzzerSounds.h"
 
-char ssid[] = SECRET_SSID;
-char pass[] = SECRET_PASS;
 int status = WL_IDLE_STATUS;
-char server[] = "meredith.vercel.app";
 
 WiFiSSLClient client;
 
@@ -21,7 +18,7 @@ const int buzzer = 15;
 const int button_health_check = 13;
 const int led_big_button = 14;
 const int big_button_long_press_limit = 5000;
-const int default_trigger_alert_limit = 10000;
+const int default_trigger_alert_limit = 50000;
 const int health_check_time_between = 3600000;
 const int override_length = 60000;
 const int common_loop_delay = 250;
@@ -38,7 +35,6 @@ struct TimedTriggerConfig {
 // Configurations for existing triggers
 TimedTriggerConfig trigger_left = {3, 19, 21, 0, default_trigger_alert_limit};
 TimedTriggerConfig trigger_right = {4, 16, 20, 0, default_trigger_alert_limit};
-
 
 unsigned long healthCheckLastSendTime = 0;
 unsigned long bigButtonPushDuration = 0;
@@ -62,6 +58,8 @@ void setup() {
   setupTrigger(trigger_right);
 
   connect_to_wifi();
+  sendHealthCheck(true);
+
 }
 
 /**
@@ -87,15 +85,24 @@ void loop() {
   delay(common_loop_delay);
 }
 
+/**
+ * Manage the ending of the timer and blinking the lights while timer is running
+ */
 void manageTriggerOverrideTimer() {
+  // check if the override timer has reached its max. If so, end the override
   if (shouldOverrideTimedTriggers && millis() - triggerOverrideStartTime > override_length) {
     triggerOverrideStartTime = 0;
     shouldOverrideTimedTriggers = false;
+    Serial.print("kill the override");
   }
+
+  // if we are within the override timeframe blink the lights
   if (shouldOverrideTimedTriggers && millis() - triggerOverrideStartTime < override_length) {
+    Serial.print("maintain the override");
     digitalWrite(trigger_left.led_success, HIGH);
     digitalWrite(trigger_right.led_success, HIGH);
     
+    // use the common loop delay so our blinking looks nice and consistent
     delay(common_loop_delay);
     
     digitalWrite(trigger_left.led_success, LOW);
@@ -104,6 +111,9 @@ void manageTriggerOverrideTimer() {
   }
 }
 
+/**
+ * The big button triggers the health check for short presses and triggers the override with long presses
+ */
 void handleBigButton() {
   if (digitalRead(button_health_check) == HIGH) {
     Serial.print("big button pushed and bigButtonPushDuration is: ");
@@ -118,7 +128,7 @@ void handleBigButton() {
     // if the button has been held down for the long press limit (or one second longer to account for other delays) and we've not yet overridden the timmedTriggers
     // then initiate the override
     if (millis() - bigButtonPushDuration > big_button_long_press_limit && !shouldOverrideTimedTriggers) {
-      Serial.println("limit reached");
+      Serial.println("limit reached - first");
       cute.play(S_DISGRUNTLED);
       shouldOverrideTimedTriggers = true;
       triggerOverrideStartTime = millis();
@@ -128,8 +138,8 @@ void handleBigButton() {
     // if the button has been held down for the long press limit (or one second longer to account for other delays) and we've already overridden the timmedTriggers
     // then we need to kill the override. But we also need to make sure that the user has not just finished
     // trying to trigger the override 1/2 a second earlier
-    if (millis() - bigButtonPushDuration > big_button_long_press_limit && shouldOverrideTimedTriggers) {
-      Serial.println("limit reached");
+    if (millis() - bigButtonPushDuration > (big_button_long_press_limit + 3000) && shouldOverrideTimedTriggers) {
+      Serial.println("limit reached - second");
       cute.play(S_DISGRUNTLED);
       shouldOverrideTimedTriggers = false;
       triggerOverrideStartTime = 0;
@@ -153,11 +163,12 @@ void handleBigButton() {
   }
 }
 
+/**
+ * Manage the firing of the https health check
+ * @param override=false - flag for forcing the triggering of the health check
+ */
 void sendHealthCheck(bool override) {
   if (override || (millis() - healthCheckLastSendTime > health_check_time_between)) {
-    Serial.print("health check fired and override was: ");
-    Serial.println(override);
-    
     cute.play(S_CONNECTION);
 
     digitalWrite(trigger_left.led_success, HIGH);
@@ -189,6 +200,8 @@ void checkTrigger(TimedTriggerConfig& config) {
       digitalWrite(config.led_error, HIGH);
       cute.play(S_CONNECTION);
       delay(2000); // simulate an http request delay
+      digitalWrite(config.led_success, LOW);
+      digitalWrite(config.led_error, LOW);
     }
   } else {
     // Trigger is not pressed, reset the start time
@@ -196,61 +209,59 @@ void checkTrigger(TimedTriggerConfig& config) {
   }
 }
 
+/**
+ * Beging and wait for the wifi connection to be completed
+ */
 void connect_to_wifi() {
   if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    cute.play(S_DISCONNECTION);
-    cute.play(S_DISGRUNTLED);
-    cute.play(S_DISGRUNTLED);
+    Serial.println("WiFi module comm failure");
+
     while (true);
   }
 
   while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass);
+    Serial.print("Attempting to connect to wifi network: ");
+    Serial.println(SECRET_SSID);
+    status = WiFi.begin(SECRET_SSID, SECRET_PASS);
     delay(10000);
   }
-
-  Serial.println("Connected to WiFi");
-  cute.play(S_CONNECTION);
-  delay(250);
-  cute.play(S_DISCONNECTION);
-  delay(250);
-  cute.play(S_CONNECTION);
   print_wifi_status();
 }
 
+/**
+ * Print out the wifi status
+ */
 void print_wifi_status() {
+  Serial.println("Connected to WiFi");
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
   IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
+  Serial.print("IP: ");
   Serial.println(ip);
 
   long rssi = WiFi.RSSI();
-  Serial.print("Signal Strength (RSSI):");
+  Serial.print("Signal strength: ");
   Serial.print(rssi);
   Serial.println(" dBm");
+  Serial.println("Pausing to allow time for attaching monitor");
+  delay(10000);
+  Serial.println("Pause complete");
 }
 
 void send_http_request() {
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
-  client.stop();
-
   Serial.println("\nStarting connection to server...");
-  if (client.connect(server, 443)) {
+
+  if (client.connect(SECRET_SERVER, 443)) {
     cute.play(S_BUTTON_PUSHED);
     Serial.println("connected to server");
 
-    client.println("GET /api/example?query=123 HTTP/1.1");
-    client.println("Host: meredith.vercel.app");
+    client.print("GET ");
+    client.print(SECRET_HEALTH_PATH);
+    client.println(" HTTP/1.1");
+    client.print("Host: ");
+    client.println(SECRET_HOST_NAME);
     client.println("Connection: close");
-    client.println("Content-Type: application/json");
   
     if (client.println() == 0) {
       Serial.println(F("Failed to send request"));
@@ -258,11 +269,12 @@ void send_http_request() {
       return;
     }
 
-    char status[32] = {0};
-    client.readBytesUntil('\r', status, sizeof(status));
-    if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+    char reqStatus[32] = {0};
+    client.readBytesUntil('\r', reqStatus, sizeof(reqStatus));
+
+    if (strncmp(reqStatus, "HTTP/1.1 201 Created", 15) != 0) {
       Serial.print(F("Unexpected response: "));
-      Serial.println(status);
+      Serial.println(reqStatus);
       cute.play(S_DISGRUNTLED);
       return;
     }
@@ -274,31 +286,24 @@ void send_http_request() {
       return;
     }
 
-    char end_of_headers2[] = "\r";
-    if (!client.find(end_of_headers2)) {
-      Serial.println(F("Invalid response2"));
-      cute.play(S_DISGRUNTLED);
-      return;
+    // Happy path. Print the entire server response, including headers and body
+    Serial.println("Server Response:");
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
     }
 
-    const size_t capacity = JSON_OBJECT_SIZE(12) + 170;
-    StaticJsonDocument<capacity> doc;
-
-    DeserializationError error = deserializeJson(doc, client);
-    if (error) {
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.c_str());
-      cute.play(S_DISGRUNTLED);
-      return;
-    }
-
-    Serial.println("no errors. Assume success");
     cute.play(S_MODE1);
     cute.play(S_CONNECTION);
     cute.play(S_MODE1);
   } else {
-    Serial.println("connection failed");
+    Serial.print("Connection failed for unknown reason. Debugging needed. WL_IDLE_STATUS: ");
+    Serial.println(WL_IDLE_STATUS);
+    // trigger two S_DISGRUNTLED so we can notify about  the unknown error
+    // TODO prob need to add WIFI reconnect logic here - see https://forum.arduino.cc/t/arduino-wifi-rev2-reconnecting-to-wifi-after-disconnected/1022789
     cute.play(S_DISGRUNTLED);
     cute.play(S_DISGRUNTLED);
   }
+
+  client.stop();
 }
